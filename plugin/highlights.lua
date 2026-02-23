36908269dd7da2236b23f192c561c88a9b3bdaa0
Barinderpreet Singh <64461700+knownasnaffy@users.noreply.github.com>
2026-02-23 14:17:57 +0530

    fix: cursorline highlighting across all columns
    - Extract only foreground colors from linked highlight groups
    - Don't set background so cursorline shows through
    - Fixes issue where subject field wasn't showing cursorline highlight
    - Cursorline now highlights the entire row uniformly

--- plugin/highlights.lua
+++ plugin/highlights.lua
@@ -1,6 +1,7 @@
 -- Highlight groups for himalaya.nvim
-vim.api.nvim_set_hl(0, "HimalayaFlag", { link = "Special" })
-vim.api.nvim_set_hl(0, "HimalayaFrom", { link = "Identifier" })
-vim.api.nvim_set_hl(0, "HimalayaSubject", { link = "Normal" })
-vim.api.nvim_set_hl(0, "HimalayaDate", { link = "Comment" })
-vim.api.nvim_set_hl(0, "HimalayaFolder", { link = "Directory" })
+-- Don't set bg so cursorline shows through
+vim.api.nvim_set_hl(0, "HimalayaFlag", { fg = vim.api.nvim_get_hl(0, { name = "Special" }).fg })
+vim.api.nvim_set_hl(0, "HimalayaFrom", { fg = vim.api.nvim_get_hl(0, { name = "Identifier" }).fg })
+vim.api.nvim_set_hl(0, "HimalayaSubject", { fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg })
+vim.api.nvim_set_hl(0, "HimalayaDate", { fg = vim.api.nvim_get_hl(0, { name = "Comment" }).fg })
+vim.api.nvim_set_hl(0, "HimalayaFolder", { fg = vim.api.nvim_get_hl(0, { name = "Directory" }).fg })
